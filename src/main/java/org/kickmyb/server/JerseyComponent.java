48eb5b3f99f2e7eac34e5e33febb4ad4dd38538c
Joris deguet <joris.deguet@gmail.com>
2020-12-25 11:26:13 -0500

    Change in way to register jersey web services for Heroku
    https://stackoverflow.com/questions/30406082/spring-boot-jersey-maven-failed-to-run-war-file
    
    suggests that this is the source (package scanning) of deployment error.

--- src/main/java/org/kickmyb/server/JerseyComponent.java
+++ src/main/java/org/kickmyb/server/JerseyComponent.java
@@ -1,6 +1,8 @@
 package org.kickmyb.server;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.kickmyb.server.id.WebServiceID;
+import org.kickmyb.server.tasks.WebServiceCore;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.ApplicationPath;
@@ -12,6 +14,10 @@ import javax.ws.rs.ApplicationPath;
 @ApplicationPath("/api/*")                              // TODO change to fix the prefix for all services, remove if none
 public class JerseyComponent extends ResourceConfig {
     public JerseyComponent() {
-        packages("org.kickmyb.server");        // TODO change if you change your package name
+
+        register(WebServiceID.class);
+        register(WebServiceCore.class);
+        // Will make deployment fail but not local run come on man!!!
+        //packages("org.kickmyb.server");        // TODO change if you change your package name
     }
 }
