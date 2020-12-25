5e5f7a185b6a53d0b6ad94340a186d9161af243b
Joris deguet <joris.deguet@gmail.com>
2020-12-25 11:32:19 -0500

    I forgot two Jersey annotated objects

--- src/main/java/org/kickmyb/server/JerseyComponent.java
+++ src/main/java/org/kickmyb/server/JerseyComponent.java
@@ -2,6 +2,8 @@ package org.kickmyb.server;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.kickmyb.server.id.WebServiceID;
+import org.kickmyb.server.jersey.CatchAllMapper;
+import org.kickmyb.server.jersey.GSONMapper;
 import org.kickmyb.server.tasks.WebServiceCore;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +12,17 @@ import javax.ws.rs.ApplicationPath;
 // Ce composant permet à Spring d'intégrer les éléments de Jersey
 // Chaque service de Jersey et lui même détecter via une annotation
 
+// TODO enlever jersey Spring a déjà ce qu'il faut donc...  Exception mapping and Gson
+
 @Component
 @ApplicationPath("/api/*")                              // TODO change to fix the prefix for all services, remove if none
 public class JerseyComponent extends ResourceConfig {
     public JerseyComponent() {
-
+        // register each class that has endpoints in them
         register(WebServiceID.class);
         register(WebServiceCore.class);
+        register(GSONMapper.class);
+        register(CatchAllMapper.class);
         // Will make deployment fail but not local run come on man!!!
         //packages("org.kickmyb.server");        // TODO change if you change your package name
     }
