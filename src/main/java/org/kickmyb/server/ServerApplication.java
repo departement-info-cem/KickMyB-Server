d6a302c0493d5d92c14d85eb12997c36dbd8c4be
Joris deguet <joris.deguet@gmail.com>
2020-12-26 13:41:08 -0500

    This is the solution to disable Jackson
    I tried many web pages and this is the only one that worked

--- src/main/java/org/kickmyb/server/ServerApplication.java
+++ src/main/java/org/kickmyb/server/ServerApplication.java
@@ -2,13 +2,15 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
 // TODO utiliser Spring security pour gérer les comptes
 // TODO implémenter le paging pour une liste très longue ????
 // TODO compléter événements pour un bébé
 // TODO regarder déploiement sur AWS ou Heroku ou autre
 
-@SpringBootApplication
+	
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class ServerApplication {
 
 	public static void main(String[] args) {
