6a52a1405d9ef7d6636791ad52266accb6e586e1
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2025-09-11 09:39:13 -0400

    Fix firebase

--- src/main/java/org/kickmyb/serveur/config/ConfigFirebase.java
+++ src/main/java/org/kickmyb/serveur/config/ConfigFirebase.java
@@ -3,6 +3,9 @@ package org.kickmyb.serveur.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.kickmyb.serveur.tache.ServicePlanificationNotifications;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,8 @@ import java.io.IOException;
 
 @Configuration
 public class ConfigFirebase {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServicePlanificationNotifications.class);
 
     @Value("${firebase.config.path:}")
     private String firebaseConfigPath;
@@ -41,7 +46,7 @@ public class ConfigFirebase {
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException("Erreur lors de l'initialisation de Firebase: " + e.getMessage(), e);
+            logger.warn("Firebase n'est pas configuré.");
         }
     }
 }
